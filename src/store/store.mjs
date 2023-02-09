2a29275a6f2020e0f5277bd6abff69ead5b1e227
christophe bragard <christophe.bragard@e-xode.net>
2023-02-09 20:31:47 +0100

    [development] metas

--- src/store/store.mjs
+++ src/store/store.mjs
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
+import metas from '@/store/modules/metas.mjs'
 import user from '@/store/modules/user.mjs'
 
 const store = createStore({
     modules: {
+        metas,
         user
     },
     plugins: []
